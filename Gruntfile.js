3533ce0c2ff36a977f9da205cc3b1c46ffc73657
ItsJonQ <itsjonq@gmail.com>
2016-02-01 16:22:58 -0500

    Updates

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) { 'use strict';
+
+  require('time-grunt')(grunt);
+
+  require('load-grunt-tasks')(grunt);
+
+  var config = {
+    app: 'app',
+    assetsDir: '{{site.theme.link}}/',
+    dist: 'public',
+    liveReload: 42526,
+    port: 80,
+    rsyncHost: 'root@162.243.32.19',
+    src: 'src',
+    temp: '.tmp',
+    theme: 'tneme-name',
+    url: 'url.dev',
+  };
+
+  grunt.initConfig({
+
+    config: config,
+
+    rsync: {
+      options: {
+        args: ['--verbose'],
+        exclude: [
+        '.git*',
+        '*.scss',
+        'node_modules',
+        '.sass-cache',
+        'app',
+        'bower_components',
+        'dist',
+        'src',
+        '.DS_Store',
+        '.jshintrc',
+        '.gitignore',
+        'bower.json',
+        'README.md',
+        'package.json',
+        '.tmp',
+        'vendor'
+        ],
+        recursive: true
+      },
+      production: {
+        options: {
+          src: './_site/',
+          dest: '../var/www/html/hs',
+          host: '<%= config.rsyncHost %>',
+          delete: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('deploy', [
+    'rsync'
+  ]);
+};
